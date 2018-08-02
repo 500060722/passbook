a9a49e2b96440257d670a70537ed041dec5ced41
Prajjawal Banati <500060722@stu.upes.ac.in>
2018-08-02 19:57:10 +0530

    Commit 2 added the queries

--- program.py
+++ program.py
@@ -1,37 +1,51 @@
 import cx_Oracle
 import time
-def debit():
+import datetime
+def debit(pin):
 	con=cx_Oracle.connect("SYSTEM/user123@localhost/xe")
 	cur=con.cursor()
-	cur.execute("")#Query#
+	cur.execute("SELECT Balance,Account_no from Accounts where PIN = :pass",{'pass':pin})#Query#
+	for line in cur:
+		balance=line[0]
+		account_no=line[1]
+	now = datetime.datetime.now()
+	a=(now.strftime("%Y-%m-%d"))
 	#please fetch the current balance from the accounts table and store it in the variable named balance
 	print("Your current account balance is ",balance)
 	debit_amount = int(input("Enter the debit amount"))
-	updated_balance = balance-debit_amount
-	print("Wait we are processing your request......")
-	#code starts
-	cur.execute("")#Update the balance in the Accounts table
-	cur.execute("")#Insert pass_no(or to keep it simple use only account no. as a foriegn key no need of pass_no.),debit_amount,date(take any date) and debit amount
-	#code ends here
-	con.commit()
-	cur.close()
-	con.close()
-	time.sleep(10)
-	print("Done")
-	print("Your now updated balance is ",updated_balance)
+	if (debit_amount<balance):
+		updated_balance = balance-debit_amount
+		print("Wait we are processing your request......")
+		#code starts
+		cur.execute("UPDATE Accounts SET Balance = :balance where PIN = :pin",{'balance':updated_balance,'pin':pin})#Update the balance in the Accounts table
+		cur.execute("INSERT into passbook1 values(:account_no ,:date ,:debit_amount)",{'account_no':account_no,'date':a,'debit_amount':debit_amount})#Insert Query#
+		#code ends
+		con.commit()
+		cur.close()
+		con.close()
+		time.sleep(10)
+		print("Done")
+		print("Your now updated balance is ",updated_balance)
+	else:
+		print("Your balance is insufficient to complte the transcation.\n")
 	
-def credit():
+def credit(pin):
 	con=cx_Oracle.connect("SYSTEM/user123@localhost/xe")
 	cur=con.cursor()
-	cur.execute("")#Query#
+	cur.execute("SELECT Balance,Account_no from Accounts where PIN = :pass",{'pass':pin})
+	for line in cur:
+		balance=line[0]
+		account_no=line[1]
+	now = datetime.datetime.now()
+	a=(now.strftime("%Y-%m-%d"))
 	#please fetch the current balance from the accounts table and store it in the variable named balance
 	print("Your current account balance is ",balance)
 	credit_amount = int(input("Enter the amount to be credited in the account"))
 	credit_balance=balance+credit_amount
 	print("Wait we are processing your request......")
 	#code starts
-	cur.execute("")#Update the balance in the Accounts table
-	cur.execute("")#Insert pass_no(or to keep it simple use only account no. as a foriegn key no need of pass_no.),debit_amount,date(take any date) and debit amount
+	cur.execute("UPDATE Accounts SET Balance = :balance where PIN = :pin",{'balance':credit_balance,'pin':pin})#Update the balance in the Accounts table
+	cur.execute("INSERT into passbook2 values(:account_no ,:date ,:credit_amount)",{'account_no':account_no,'date':a,'credit_amount_amount':credit_amount})
 	#code ends
 	con.commit()
 	cur.close()
@@ -62,15 +76,15 @@ def identification():
 		cur.execute("select ACCOUNTNO from ACCOUNTS where PIN =:pin",{'pin':pin})
 		for line in cur:
 				account_no=line[0]
-		print("So your account no is :",account_no)
+		print("So your account no is :,"account_no)
 		print("So please select the following:\n1>DEBIT THE AMOUNT\n2>CREDIT (DEPOSIT) THE AMOUNT\n3>PASSBOOOK PRINTING")
 		i=int(input("Enter your choice: "))
 		if (i==1):
-			debit()
+			debit(pin)
 		if (i==2):
-			credit()
+			credit(pin)
 		if (i==3):
-			passbook()
+			passbook(pin)
 		if (i>3):
 			print("Wrong choice....")
 	except cx_Oracle.DatabaseError as e:
